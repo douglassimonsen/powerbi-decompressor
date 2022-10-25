b47c9b9e7f53a81861e71f8b36de7f36e4eacaea
Matthew Hamilton <daedalus1432@gmail.com>
2022-10-24 19:39:03 -0500

    Switched to using jinja2. Can now update multiple tables simultaneously.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 
 def main():
     pbi = powerbi.PowerBi(r"C:\Users\mwham\Documents\repos\test-decompressor\api.pbix")
-    pbi.update_table('Kris')
+    print(pbi.update_tables())
     exit()
     pbi.save_image(r"C:\Users\mwham\Documents\a.pbix")
 
--- powerbi.py
+++ powerbi.py
@@ -7,8 +7,8 @@ import uuid
 import parse_schema
 import shutil
 import initialization
-
-xmls = {f[:-4]: open(f"xmla/{f}").read() for f in os.listdir("xmla")}
+import jinja2
+xmls = {f[:-4]: jinja2.Template(open(f"xmla/{f}").read()) for f in os.listdir("xmla")}
 
 
 class PowerBi:
@@ -30,27 +30,35 @@ class PowerBi:
     def load_image(self):
         with Pyadomd(self.conn_str) as conn:  # need to generate a random GUID
             conn.cursor().executeXML(
-                xmls["image_load"].format(guid=self.guid, source_path=self.source_path)
+                xmls["image_load"].render(guid=self.guid, source_path=self.source_path)
             )
 
     def save_image(self, target_path):
         shutil.copy(self.source_path, target_path)  # needs a PBIX to save the datamodel into
         with Pyadomd(self.conn_str) as conn:
             x = conn.cursor().executeXML(
-                xmls["image_save"].format(guid=self.guid, target_path=target_path)
+                xmls["image_save"].render(guid=self.guid, target_path=target_path)
             )
 
     def read_schema(self):
         with Pyadomd(self.conn_str) as conn:
-            schema = conn.cursor().executeXML(xmls["schema_query"].format(guid=self.guid))
+            schema = conn.cursor().executeXML(xmls["schema_query"].render(guid=self.guid))
         self.schema = parse_schema.parse_schema(schema)
         return self.schema
 
-    def update_table(self, table_name):
+    def update_tables(self, table_names=None):
         if not self.schema:
             self.read_schema()
 
         table_dict = {t['Name']: t['ID'] for t in self.schema['Table']}
-        table_id = table_dict[table_name]
+        if isinstance(table_names, str):
+            table_ids = [table_dict[table_names]]
+        elif isinstance(table_names, list):
+            table_ids = [table_dict[t] for t in table_names]
+        elif table_names is None:
+            table_ids = list(table_dict.values())
+        else:
+            raise TypeError("I don't understand the object: ", table_names)
+
         with Pyadomd(self.conn_str) as conn:
-            ret = conn.cursor().executeXML(xmls["refresh_object"].format(guid=self.guid, table_id=table_id))
+            return conn.cursor().executeXML(xmls["update_table"].render(guid=self.guid, table_ids=table_ids))
