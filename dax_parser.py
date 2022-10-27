fb098a3a76e9e89a316b531c780f83c19e7fa7c6
Matthew Hamilton <daedalus1432@gmail.com>
2022-10-26 23:31:04 -0500

    Beginning to transform

--- dax_parser.py
+++ dax_parser.py
@@ -9,14 +9,23 @@ import lark
 
 expression = r'''
 let
-    Source = Excel.Workbook(File.Contents("C:\Users\mwham\Desktop\test.xlsx"), null, true),
-    Kris_Sheet = Source{[Item="Kris",Kind="Sheet"]}[Data],
-    #"Promoted Headers" = Table.PromoteHeaders(Kris_Sheet, [PromoteAllScalars=true]),
-    #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"a", Int64.Type}, {"b", Int64.Type}})
+    Source = List.Generate(() =>
+  [Result = try country(1) otherwise null, Page=1],
+  each [Result] <> null,
+  each [Result = try country([Page] + 1) otherwise null, Page=[Page] + 1],
+  each [Result]
+),
+    #"Converted to Table" = Table.FromList(Source, Splitter.SplitByNothing(), null, null, ExtraValues.Error),
+    #"Expanded Column1" = Table.ExpandTableColumn(#"Converted to Table", "Column1", {"iso2Code", "name", "region", "adminregion", "incomeLevel", "lendingType", "capitalCity", "longitude", "latitude", "Attribute:id"}, {"Column1.iso2Code", "Column1.name", "Column1.region", "Column1.adminregion", "Column1.incomeLevel", "Column1.lendingType", "Column1.capitalCity", "Column1.longitude", "Column1.latitude", "Column1.Attribute:id"}),
+    #"Column1 region" = #"Expanded Column1"{11}[Column1.region],
+    #"Changed Type" = Table.TransformColumnTypes(#"Column1 region",{{"Element:Text", type text}, {"Attribute:id", type text}, {"Attribute:iso2code", type text}})
 in
     #"Changed Type"
 '''
 expression = r'''
+Calendar(Date(2015,1,1), Date(2015,1,1))
+'''
+expression = r"""
 let
     Source = Excel.Workbook(File.Contents("C:\Users\mwham\Desktop\test.xlsx"), null, true),
     Kris_Sheet = Source{[Item="Kris",Kind="Sheet"]}[Data],
@@ -24,9 +33,6 @@ let
     #"Changed Type" = Table.TransformColumnTypes(#"Promoted Headers",{{"a", Int64.Type}, {"b", Int64.Type}})
 in
     #"Changed Type"
-'''
-expression = r"""
-    Kris_Sheet = Source{[Item="Kris",Kind="Sheet"]}[Data],
 """
 l = lark.Lark('''
     start: line*
@@ -34,7 +40,7 @@ l = lark.Lark('''
     statement: variable "=" command
 
 
-    command: function | source_filter ","?
+    command: (function | source_filter) ","?
     function: /[\w\.]+/ "(" (function | arg)* ")"
     arg: /[^\(\)]+/
 
@@ -48,9 +54,42 @@ l = lark.Lark('''
     %ignore WS
 ''')
 
+
+class ExcelSource:
+    def __init__(self, source_path=None, source_tab=None):
+        self.source_path = source_path
+        self.source_tab = source_tab
+
+
+
+
+class RPN(lark.Transformer):
+    def __init__(self):
+        source_var = None
+
+    def command(self, args):
+        if args[0].children[0] == 'Excel.Workbook':
+            source_loc = args[0].children[1].children
+            if source_loc[0] == 'File.Contents':
+                source_path = source_loc[1].children[0].value
+            
+            self.source_var = ExcelSource(source_path=source_path)
+
+        else:
+            print(args[0])
+
+    def start(self, args):
+        return args
+  
+
 x = l.parse(expression)
+RPN().transform(x)
+exit()
+
 for n in x.iter_subtrees():
     if n.data != 'command':
         continue
-    print(n.children[0])
+    if n.children[0].children[0] == 'Excel.Workbook':
+        pass
+    print()
     exit()
