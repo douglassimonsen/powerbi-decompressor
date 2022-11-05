398955805268686b1a3b8c329c21f918c5e1f9d4
Matthew Hamilton <daedalus1432@gmail.com>
2022-11-04 19:40:05 -0500

    Getting sources from visuals

--- app/parse_visuals.py
+++ app/parse_visuals.py
@@ -5,22 +5,37 @@ from pprint import pprint
 with zipfile.ZipFile('api.pbix') as zf:
     data = json.loads(zf.open('Report/Layout', 'r').read().decode('utf-16-le'))
 
+
+def find_source(data):
+    if not isinstance(data, dict):
+        return
+    if 'SourceRef' in data.get('Expression', {}):
+        return data
+    for v in data.values():            
+        ret = find_source(v)
+        if ret is not None:
+            return ret
+
+
 for section in data['sections']:
     section_info = {
         'section_name': section['name'],
         'ordinal': section['ordinal'],
     }
     for visual in section['visualContainers']:
-        visual = {
+        ret = {
             'height': visual['height'],
-            'weight': visual['weight'],
+            'width': visual['width'],
             'x': visual['x'],
             'y': visual['y'],
             'z': visual['z'],
         }
-        # dataTransforms
         # filters
         # query
         # config
-        pprint(visual['dataTransforms'])
-        exit()
+        ret['filters'] = []
+        ret['selects'] = []
+        for f in json.loads(visual['dataTransforms'])['queryMetadata']['Filters']:
+            ret['filters'].append(find_source(f))
+        for f in json.loads(visual['dataTransforms'])['selects']:
+            ret['selects'].append(find_source(f))
