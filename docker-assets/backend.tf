4f287ccdf4f1793742cab40d9c37dac5261a5573
Matthew Hamilton <daedalus1432@gmail.com>
2023-01-05 23:47:05 -0600

    backend is working!!

--- docker-assets/backend.tf
+++ docker-assets/backend.tf
@@ -1,34 +1,32 @@
+resource "aws_api_gateway_rest_api" "backend" {
+  name = "visualizer-backend"
+}
+resource "aws_api_gateway_resource" "backend" {
+  rest_api_id = "${aws_api_gateway_rest_api.backend.id}"
+  parent_id   = "${aws_api_gateway_rest_api.backend.root_resource_id}"
+  path_part   = "{proxy+}"
+}
+resource "aws_api_gateway_method" "backend" {
+  rest_api_id   = "${aws_api_gateway_rest_api.backend.id}"
+  resource_id   = "${aws_api_gateway_resource.backend.id}"
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "backend" {
+  rest_api_id = "${aws_api_gateway_rest_api.backend.id}"
+  resource_id = "${aws_api_gateway_method.backend.resource_id}"
+  http_method = "${aws_api_gateway_method.backend.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "${aws_lambda_function.backend.invoke_arn}"
+}
 resource "aws_api_gateway_deployment" "backend" {
   rest_api_id = aws_api_gateway_rest_api.backend.id
   stage_name  = "Stage"
-}
-resource "aws_api_gateway_rest_api" "backend" {
-  name = "visualizer-backend"
-  body = jsonencode({
-    "info" : {
-      "version" : "1.0",
-      "title" : "visualizer-backend"
-    },
-    "paths" : {
-      "/{proxy+}" : {
-        "x-amazon-apigateway-any-method" : {
-          "x-amazon-apigateway-integration" : {
-            "httpMethod" : "ANY",
-            "type" : "aws_proxy",
-            "uri" : "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:111263457661:function:sam-app2-LambdaFunctionOverHttp-M4mBgJtmTNzx/invocations"
-          },
-
-          "responses" : {}
-        }
-      }
-    },
-    "swagger" : "2.0"
-  })
-}
-resource "aws_api_gateway_stage" "backend" {
-  stage_name    = "Stage"
-  rest_api_id   = aws_api_gateway_rest_api.backend.id
-  deployment_id = aws_api_gateway_deployment.backend.id
+  depends_on = [
+    aws_api_gateway_integration.backend,
+  ]
 }
 resource "aws_lambda_function" "backend" {
   filename      = "lambda.zip"
@@ -37,6 +35,29 @@ resource "aws_lambda_function" "backend" {
   role          = aws_iam_role.backend.arn
   timeout       = 5
   handler       = "application.lambda_handler"
+}
+resource "aws_iam_policy" "backend" {
+  name = "lambda-demo"
+  path = "/"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "asd",
+        "Effect" : "Allow",
+        "Action" : "ssm:GetParameter",
+        "Resource" : ["arn:aws:ssm:us-east-1:111263457661:parameter/db"]
+      },
+      {
+        "Sid" : "qwe",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:DescribeParameters"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
 }
 resource "aws_iam_role" "backend" {
   assume_role_policy = jsonencode({
@@ -55,11 +76,18 @@ resource "aws_iam_role" "backend" {
       }
     ]
   })
-  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"]
+  
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole", aws_iam_policy.backend.arn]
 }
 resource "aws_lambda_permission" "backend" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.backend.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:us-east-1:111263457661:${aws_api_gateway_rest_api.backend.arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.backend.execution_arn}/*/*"
+}
+resource "aws_ssm_parameter" "backend_url" {
+  name = "visualizer-backend"
+  type = "String"
+  value = aws_api_gateway_deployment.backend.invoke_url
 }
